f2d99dae7252936a76bc1ffbe28e03e3f4b15a57
sombatp <sombatp@orionhealth.com>
2021-07-17 20:30:27 +0700

    Modified FirstApp

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
 main() => runApp(MyApp());
 
@@ -23,77 +22,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  var ctl_radius = TextEditingController();
-  var result = '-----Result-----';
-
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Row(children: [
-          Center(
-            child: Text(
-              'เเอปคำนวณพื้นที่วงกลม',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 40.0,
-                  fontFamily: 'SdProtector',
-                  color: Colors.blue[900]),
-            ),
-          ),
-          Icon(
-            Icons.calculate_rounded,
-            size: 80,
-            color: Colors.orange,
-          )
-        ], mainAxisAlignment: MainAxisAlignment.center),
-        Padding(
-          padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
-          child: Image.asset('assets/circle.png', scale: 0.5),
-        ),
-        Padding(
-            padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
-            child: TextField(
-              controller: ctl_radius,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: 'รัศมี', border: OutlineInputBorder()),
-            )),
-        Padding(
-          padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
-          child: ElevatedButton(
-            style: raiseButtonStyle,
-            onPressed: () {
-              var radius = int.parse(ctl_radius.text);
-              var area = math.pi * (radius * radius);
-              print("Area: $area");
-              setState(() {
-                result = "ผลลัพธ์\nรัศมี: $radius\nพื้นที่วงกลม: $area";
-              });
-            },
-            child: Text(
-              'คำนวณ',
-              style: TextStyle(fontSize: 20.0),
-            ),
-          ),
-        ),
-        Padding(
-            padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
-            child: Text(
-              result,
-              style: TextStyle(fontSize: 30.0),
-            )),
-      ],
+    return Scaffold(
+      body: Center(
+          child: Text(
+        "bye bye covid",
+        style: TextStyle(
+            fontSize: 50, color: Colors.orange[200], fontFamily: "Passadoo"),
+      )),
     );
   }
 }
 
-final ButtonStyle raiseButtonStyle = ElevatedButton.styleFrom(
-  onPrimary: Colors.blue[400],
-  primary: Colors.blue[900],
-  minimumSize: Size(88, 50),
-  padding: EdgeInsets.symmetric(horizontal: 16),
-  shape: const RoundedRectangleBorder(
-    borderRadius: BorderRadius.all(Radius.circular(5)),
-  ),
-);
+// final ButtonStyle raiseButtonStyle = ElevatedButton.styleFrom(
+//   onPrimary: Colors.blue[400],
+//   primary: Colors.blue[900],
+//   minimumSize: Size(88, 50),
+//   padding: EdgeInsets.symmetric(horizontal: 16),
+//   shape: const RoundedRectangleBorder(
+//     borderRadius: BorderRadius.all(Radius.circular(5)),
+//   ),
+// );
