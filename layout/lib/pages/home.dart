efce76b9a913ba96f37d32c2f5e3d9f571ddfcd6
sombatp <sombatp@orionhealth.com>
2021-09-05 15:28:40 +0700

    Modified layout use data.json on github

--- layout/lib/pages/home.dart
+++ layout/lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:layout/pages/detail.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   //const HomePage({ Key? key }) : super(key: key);
@@ -19,19 +21,21 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
           padding: const EdgeInsets.all(20),
           child: FutureBuilder(
-            builder: (context, snapshot) {
-              var data = json.decode(snapshot.data.toString()); // [{},{},{}]
+            builder: (context, AsyncSnapshot snapshot) {
+              //var data = json.decode(snapshot.data.toString()); // [{},{},{}]
 
               return ListView.builder(
                 itemBuilder: (BuildContext context, int index) {
-                  return MyBox(data[index]['title'], data[index]['subtitle'],
-                      data[index]['image_url'], data[index]['detail']);
+                  return MyBox(snapshot.data[index]['title'], snapshot.data[index]['subtitle'],
+                      snapshot.data[index]['image_url'], snapshot.data[index]['detail']);
                 },
-                itemCount: data.length,
+                itemCount: snapshot.data.length,
               );
             },
-            future:
-                DefaultAssetBundle.of(context).loadString('assets/data.json'),
+            // future:
+            //     DefaultAssetBundle.of(context).loadString('assets/data.json'),
+            future: getData(), // will get data (data.json) from github
+
           )),
     );
   }
@@ -88,10 +92,17 @@ class _HomePageState extends State<HomePage> {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) =>
-                                DetailPage(v1, v2, v3, v4)));
+                            builder: (context) => DetailPage(v1, v2, v3, v4)));
                   },
                   child: Text("อ่านต่อ"))
             ]));
+  }
+
+  Future getData() async {
+    //https://raw.githubusercontent.com/weah79/BasicAPI/main/data.json
+    var url = Uri.https('raw.githubusercontent.com', '/weah79/BasicAPI/main/data.json');
+    var response = await http.get(url);
+    var result = json.decode(response.body);
+    return result;
   }
 }
